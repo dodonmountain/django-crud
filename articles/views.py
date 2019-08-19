37e9d523a710d801ff44bf76b528ff57b561e111
dodonmountain <lkh151515@gmail.com>
2019-08-19 15:42:49 +0900

    03| articles index

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from .models import Article
 # Create your views here.
+def index(request):
+    articles = Article.objects.all()
+    titles = []
+    contents = []
+    for i in articles:
+        titles.append(i.title)
+        contents.append(i.content)
+
+    context = {
+        'articles': articles,
+        'titles' : titles,
+        'contents' : contents,
+    }
+    return render(request, 'articles/index.html', context)
