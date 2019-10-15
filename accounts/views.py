9c60d79b94a5bcd8f80dbf4f2c5c179f86e5ec55
dodonmountain <lkh151515@gmail.com>
2019-10-15 11:49:26 +0900

    Accounts signup

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+
+def signup(request):
+    if request.method == 'POST':
+        uc_form = UserCreationForm(request.POST)
+        if uc_form.is_valid():
+            account = uc_form.save()
+            return redirect('articles:index')
+    else:
+        uc_form = UserCreationForm()
+    context = {
+        'uc_form':uc_form,
+    }
+    return render(request, 'accounts/signup.html', context)
--- crud/settings.py
+++ crud/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'articles',
     'bootstrap4',
+    'accounts',
     'django.contrib.admin', # admin
     'django.contrib.auth',
     'django.contrib.contenttypes',
