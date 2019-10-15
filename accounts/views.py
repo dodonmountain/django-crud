2f7f769b2b4c2103c17bbd4d5a901a80801955f9
dodonmountain <lkh151515@gmail.com>
2019-10-15 14:09:26 +0900

    17| Signup & Login

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login as auth_login
 
 # Create your views here.
 
@@ -15,3 +16,18 @@ def signup(request):
         'uc_form':uc_form,
     }
     return render(request, 'accounts/signup.html', context)
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            # 로그인
+            user = form.get_user()
+            auth_login(request, user)
+            return redirect('articles:index')
+    else:
+        form = AuthenticationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/login.html', context)
